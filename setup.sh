84cb6f190310554688e6e61a8d5f158821c3d262
Pankaj Pansari <pankaj.pansari1@gmail.com>
2026-06-18 18:35:23 +0530

    Modify setup to use venv uv instead of global machine uv. Requirements now adds pandas

--- setup.sh
+++ setup.sh
@@ -7,24 +7,22 @@ export HF_HOME="$WORKSPACE/.cache/huggingface"
 
 mkdir -p $HF_HOME
 
-#install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-uv self update
-
 if [ ! -d "$VENV" ]; then
     echo "Creating virtual environment at $VENV"
     python3 -m venv $VENV
-    "$VENV/bin/uv" pip install --upgrade pip
-    "$VENV/bin/uv" pip install -r "$REPO/requirements.txt"
-    "$VENV/bin/uv" pip install torch --index-url https://download.pytorch.org/whl/cu124
-    "$VENV/bin/uv" pip install flashinfer-python flashinfer-cubin
-    "$VENV/bin/uv" pip install flashinfer-jit-cache --index-url https://flashinfer.ai/whl/cu124
+    
+    source "$VENV/bin/activate"
+    
+    pip install --upgrade pip uv
+    uv pip install --upgrade pip
+    uv pip install -r "$REPO/requirements.txt"
+    uv pip install torch --index-url https://download.pytorch.org/whl/cu124
+    uv pip install flashinfer-python flashinfer-cubin
+    uv pip install flashinfer-jit-cache --index-url https://flashinfer.ai/whl/cu124
 else
     echo "Virtual environment already exists at $VENV"
+    source "$VENV/bin/activate"
 fi
-
-source "$VENV/bin/activate"
 
 git config --global user.name "pankajpansari"
 git config --global user.email "pankaj.pansari1@gmail.com"
