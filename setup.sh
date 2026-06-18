ffd6cc360ebc8dd1604cf0323285ba2c277f66db
Pankaj Pansari <pankaj.pansari1@gmail.com>
2026-06-18 12:57:03 +0530

    Fix minor issue in setup relating to path for requirements.txt and quoting /bin/uv

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 export WORKSPACE=/workspace
 export VENV="$WORKSPACE/venv"
+export REPO="$WORKSPACE/microbenchmarks_green_context"
 export HF_HOME="$WORKSPACE/.cache/huggingface"
 
 mkdir -p $HF_HOME
@@ -14,11 +15,11 @@ uv self update
 if [ ! -d "$VENV" ]; then
     echo "Creating virtual environment at $VENV"
     python3 -m venv $VENV
-    "$VENV/bin/uv pip" install --upgrade pip
-    "$VENV/bin/uv pip" install -r $WORKSPACE/requirements.txt
-    "$VENV/bin/uv pip" install torch --index-url https://download.pytorch.org/whl/cu124
-    "$VENV/bin/uv pip" install flashinfer-python flashinfer-cubin
-    "$VENV/bin/uv pip" install flashinfer-jit-cache --index-url https://flashinfer.ai/whl/cu124
+    "$VENV/bin/uv" pip install --upgrade pip
+    "$VENV/bin/uv" pip install -r "$REPO/requirements.txt"
+    "$VENV/bin/uv" pip install torch --index-url https://download.pytorch.org/whl/cu124
+    "$VENV/bin/uv" pip install flashinfer-python flashinfer-cubin
+    "$VENV/bin/uv" pip install flashinfer-jit-cache --index-url https://flashinfer.ai/whl/cu124
 else
     echo "Virtual environment already exists at $VENV"
 fi
