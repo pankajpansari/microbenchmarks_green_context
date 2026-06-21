4832d86fdcfb7c85f8bd4ec5443979786dc6f01c
Pankaj Pansari <pankaj.pansari1@gmail.com>
2026-06-21 12:05:39 +0530

    Add function to overlay itl with or without prefill interference

--- plot_latency.py
+++ plot_latency.py
@@ -71,7 +71,57 @@ def plot_decode_vs_prefill_itl_delta(csv_filename1, csv_filename2):
   #ax.legend(handles=handles, title='Active SMs', loc='upper left')
 
   target_filename = os.path.join('plots', os.path.relpath(csv_filename2, 'data'))
-  target_filename = target_filename.replace('.csv', '_comparison.png')
+  target_filename = target_filename.replace('.csv', '_delta_comparison.png')
+  os.makedirs(os.path.dirname(target_filename), exist_ok=True)
+  plt.savefig(target_filename)
+  plt.close()
+
+def plot_decode_vs_prefill_itl(csv_filename1, csv_filename2, batches):
+  # Overlay ITL time (ms, per-layer) for no-contention decode and decode with prefill contention (varying prefill sizes)
+  # csv_filename1: no contention decode info; csv_filename2: decode with (serial or batched) prefills
+  df_isol = pd.read_csv(csv_filename1, comment = '#')
+
+  max_sm = max(df_isol['Active_SMs'])
+  df_isol = df_isol[(df_isol['Active_SMs'] != max_sm)]
+
+  df_with_contention = pd.read_csv(csv_filename2, comment = '#')
+
+  S_prefill_list = df_with_contention['S_prefill'].unique()
+  active_sms_list = df_with_contention['Active_SMs'].unique()[0:4]
+
+  # NOTE: We are excluding B = 32 because ITL flat across SM count; decode is launch/latency bound; comparison is not meaningful.
+  #batches = df_isol['Batch'].unique()[1:]
+  batches.sort()
+
+  #Check whether all specified batch sizes are actually present in the csv
+  assert(set(batches).issubset(set(df_isol['Batch'].unique())))
+
+  markers = {"no_prefill": 'o', "512": '^', "1024": 's', "2048": '*'}
+  cmap = plt.get_cmap('tab10')
+  color_map = {b: cmap(i) for i, b in enumerate(batches)}
+
+  fig, ax = plt.subplots()
+
+  for i, batch in enumerate(batches):
+    isol = df_isol[df_isol['Batch'] == batch]
+
+    ax.plot(isol['Active_SMs'], isol['ITL_ms'], marker = 'o', color = cmap(i), linestyle = ':')
+
+    for j, S_prefill in enumerate(S_prefill_list):
+      cont = df_with_contention[(df_with_contention['S_prefill'] == S_prefill) &
+                                (df_with_contention['Batch'] == batch)]
+      ax.plot(cont['Active_SMs'], cont['ITL_ms'], marker = markers[str(S_prefill)], color = color_map[batch], linestyle = '-')
+
+  ax.set_xlabel('Active_SMs')
+  ax.set_ylabel('Single layer decode time (ms)')
+
+  handles = [Line2D([0], [0], marker=m, color = 'black', linestyle = 'None', label=prefill_len) for prefill_len, m in markers.items()]
+  fig.legend(handles = handles, title = 'S_prefill', loc = 'outside upper right')
+  handles = [Line2D([0], [0], color = c, linestyle = '-', label= str(batch)) for batch, c in color_map.items()]
+  fig.legend(handles = handles, title = 'Batch Size', loc = 'outside right')
+
+  target_filename = os.path.join('plots', os.path.relpath(csv_filename2, 'data'))
+  target_filename = target_filename.replace('.csv', '_itl_comparison.png')
   os.makedirs(os.path.dirname(target_filename), exist_ok=True)
   plt.savefig(target_filename)
   plt.close()
